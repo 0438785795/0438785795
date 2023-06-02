3e2202d7f4204c06ec458f52a9579b0edb7db4a4
0438785795 <134633374+0438785795@users.noreply.github.com>
2023-06-02 19:33:07 +0100

    rebautofinal.lua

--- rebautofinal.lua
+++ rebautofinal.lua
@@ -521,6 +521,16 @@ end)
                                                         spawn(function()
                                                             -- Check if character is still alive before invoking move
                                                             if player.Character and player.Character:FindFirstChild("Humanoid") and player.Character.Humanoid.Health > 0 and game:GetService("Workspace").Living[player.Name].HumanoidRootPart then
+                                                            local args = {
+                                                               [1] = "Energy Volley",
+                                                               [2] = {
+                                                               ["FaceMouse"] = true,
+                                                               ["MouseHit"] = CFrame.new(humanoidRootPart.Position),
+                                                            },
+                                                               [3] = "Blacknwhite27"
+                                                           }
+
+                                                               game:GetService("ReplicatedStorage"):WaitForChild("Package"):WaitForChild("Events"):WaitForChild("voleys"):InvokeServer(unpack(args))
                                                                             
                                                                  local args = {
                                                                  [1] = "Destruction",
@@ -530,6 +540,19 @@ end)
                                                                  },
                                                                  [3] = "Blacknwhite27"
                                                                 }
+
+                                                                 game:GetService("ReplicatedStorage"):WaitForChild("Package"):WaitForChild("Events"):WaitForChild("Hak"):InvokeServer(unpack(args))
+                                                                            
+                                                                 local args = {
+                                                                 [1] = "Soul Punisher",
+                                                                 [2] = {
+                                                                 ["FaceMouse"] = true,
+                                                                 ["MouseHit"] = CFrame.new(humanoidRootPart.Position),
+                                                                 },
+                                                                 [3] = "Blacknwhite27"
+                                                                }
+
+                                                                 game:GetService("ReplicatedStorage"):WaitForChild("Package"):WaitForChild("Events"):WaitForChild("Hak"):InvokeServer(unpack(args))
 
                                                                 Event:InvokeServer(move, m6)
                                                             end
