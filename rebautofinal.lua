5abf56dbb565052026b24fa9914cf5b29eeeb272
0438785795 <134633374+0438785795@users.noreply.github.com>
2023-06-04 16:28:19 +0100

    rebautofinal.lua

--- rebautofinal.lua
+++ rebautofinal.lua
@@ -412,10 +412,11 @@ end)
                             workspace.Others.NPCs["X Fighter Trainer"],
                         }
                         local lastBoss = ""
-                        local strength = game:GetService("Workspace").Living[player.Name].Stats.Strength.Value
-                        local speed = game:GetService("Workspace").Living[player.Name].Stats.Speed.Value
-                        local defence = game:GetService("Workspace").Living[player.Name].Stats.Defense.Value
-                        local energy = game:GetService("Workspace").Living[player.Name].Stats.Energy.Value
+                        local plr = game.Players.LocalPlayer        
+                        local strength = game:GetService("ReplicatedStorage").Datas[plr.UserId].Strength.Value
+                        local speed = game:GetService("ReplicatedStorage").Datas[plr.UserId].Speed.Value
+                        local defence = game:GetService("ReplicatedStorage").Datas[plr.UserId].Defense.Value            
+                        local energy = game:GetService("ReplicatedStorage").Datas[plr.UserId].Energy.Value
     
                         while autoToggle do
                             wait()
