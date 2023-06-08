1d11d105272d15a8297a594ec3207562c68b5952
0438785795 <134633374+0438785795@users.noreply.github.com>
2023-06-08 21:10:31 +0100

    rebautofinal.lua

--- rebautofinal.lua
+++ rebautofinal.lua
@@ -365,7 +365,7 @@ end)
                                 if game:GetService("ReplicatedStorage").Datas[player.userId].Quest.Value == "" then
                                     wait(8)
                                     for _, boss in ipairs(bosses) do
-                                        if boss.Name ~= lastBoss and workspace:FindFirstChild("Living") and workspace.Living:FindFirstChild(boss.Name) and workspace.Living:FindFirstChild(player.Name) then
+                                        if boss.Name ~= lastBoss and workspace:FindFirstChild("Living") and workspace.Living:FindFirstChild(boss.Name) and workspace.Living:FindFirstChild(player.Name) and (player.Status.Transformation.Value != "") then
                                             game:GetService("ReplicatedStorage").Package.Events.Qaction:InvokeServer(boss)
                                             if game:GetService("ReplicatedStorage").Datas[player.userId].Quest.Value == boss.Name then
                                                 _G.Looping = true
