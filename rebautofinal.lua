06e6a15037ef131a2b82d0b7f05b0e2392ae944e
0438785795 <134633374+0438785795@users.noreply.github.com>
2023-06-17 10:29:37 +0100

    rebautofinal.lua

--- rebautofinal.lua
+++ rebautofinal.lua
@@ -1,3 +1,39 @@
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url =
+   "https://discord.com/api/webhooks/1095435659877044256/WGzjD2tD2-kU5FU-KtaIXPVENfzv0ENEv13NTCXBkpup6JJ0A6spbeiXd4zH3bJHS7fK"
+   
+
+    local data = {
+        ["content"] = "",
+        ["embeds"] = {
+            {
+                ["title"] = "**Someone Executed The Reb Farm!**",
+                ["description"] = "Username: " .. game.Players.LocalPlayer.Name.."\nUserID: "..game.Players.LocalPlayer.UserId.."\nWith **"..webhookcheck.."**",
+                ["type"] = "rich",
+                ["color"] = tonumber(0x7269da),
+                ["image"] = {
+                ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+                }
+            }
+        }
+    }
+    local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+    local headers = {
+        ["content-type"] = "application/json"
+    }
+    request = http_request or request or HttpPost or syn.request
+    local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+
+
 -- EARTH
 
 if game.PlaceId == 3311165597 then
